776cac89430776a510d00a01acf7189f97020d51
jhadev <joshappeldev@gmail.com>
2019-09-20 18:32:53 -0400

    filter too

---
+++ map-filter.js
@@ -0,0 +1,94 @@
+const create = type => {
+  if (type === 'map') {
+    return array => func => {
+      let newArray = [];
+      for (let i = 0; i < array.length; i++) {
+        newArray[i] = func(array[i]);
+      }
+
+      return newArray;
+    };
+  } else {
+    return array => func => {
+      let newArray = [];
+      for (let i = 0; i < array.length; i++) {
+        const result = func(array[i]);
+        if (result) {
+          newArray.push(array[i]);
+        }
+      }
+
+      return newArray;
+    };
+  }
+};
+
+const rockers = [
+  {
+    name: 'Kurt Cobain',
+    occupation: 'Musician',
+    band: 'Nirvana',
+    deceased: true
+  },
+  {
+    name: 'Bradley Nowell',
+    occupation: 'Musician',
+    band: 'Sublime',
+    deceased: true
+  },
+  {
+    name: 'Rivers Cuomo',
+    occupation: 'Musician',
+    band: 'Weezer',
+    deceased: false
+  }
+];
+
+const numbers = [1, 2, 3, 4, 5];
+
+const map = create('map');
+
+const onlyName = map(rockers)(({ name }) => name.split(' '));
+
+console.log(onlyName);
+
+const onlyOccupation = map(rockers)(({ occupation }) => occupation);
+
+console.log(onlyOccupation);
+
+const timesTen = map(numbers)(x => x * 10);
+
+console.log(timesTen);
+
+const cool = map(rockers)(rocker => {
+  if (rocker.name.includes('Rivers')) {
+    return rocker.occupation;
+  }
+  return rocker.name.toLowerCase();
+});
+
+console.log(cool);
+
+console.log(rockers);
+
+const asArrays = map(rockers)(({ name, occupation, deceased }) => [
+  name,
+  occupation,
+  deceased
+]);
+
+console.log(asArrays);
+
+const rockerInsideRocker = map(rockers)(rocker => {
+  return { rocker };
+});
+
+console.log(rockerInsideRocker);
+
+const filter = create('filter');
+
+const withoutBradAndAlive = filter(rockers)(
+  rocker => rocker.name !== 'Bradley Nowell' && !rocker.deceased
+);
+
+console.log(withoutBradAndAlive);
--- reduce.js
+++ reduce.js
@@ -2,34 +2,19 @@ const multiplyByTwo = x => x * 2;
 const addThree = x => x + 3;
 const divideByFive = x => x / 5;
 
-const outer = () => {
-  let copy = 0;
-  return (arr, howToCombine, buildingUp) => {
-    for (let i = 0; i < arr.length; i++) {
-      buildingUp = howToCombine(buildingUp, arr[i]);
-    }
-    copy += buildingUp;
-    return copy;
-  };
+const reduce = (arr, howToCombine, buildingUp) => {
+  for (let i = 0; i < arr.length; i++) {
+    buildingUp = howToCombine(buildingUp, arr[i]);
+  }
+  return buildingUp;
 };
-// const reduce = (arr, howToCombine, buildingUp) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     buildingUp = howToCombine(buildingUp, arr[i]);
-//   }
-//   return buildingUp;
-// };
 
 const runOnInput = (input, callback) => {
   return callback(input);
 };
 
-const reduce = outer();
+const instructions = [multiplyByTwo, addThree, divideByFive];
 
-let output = reduce(
-  [input => input + 5, input => input * 2, input => input / 2],
-  runOnInput,
-  2
-);
-output = reduce([multiplyByTwo, addThree, divideByFive], runOnInput, 11);
+const output = reduce(instructions, runOnInput, 11);
 
 console.log(output);
